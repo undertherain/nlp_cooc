a0de36a789036d2a8ff7015414bd910c138dc4d0
Aleksandr Drozd <alexander.drozd@gmail.com>
2017-10-13 16:03:25 +0900

    style

--- src/create_vocab.cpp
+++ src/create_vocab.cpp
@@ -24,7 +24,7 @@ typedef int64_t Index;
 #include "json.hpp"
 
 using json = nlohmann::json;
-typedef std::map<Index,Index> Accumulator;
+typedef std::map<Index, Index> Accumulator;
 std::vector<Accumulator> counters;
 json metadata;
 
@@ -49,9 +49,9 @@ std::string get_str_time()
 int main(int argc, char * argv[])
 {
     auto time_start = std::time(nullptr);
-    Options options = ProcessOptions(argc,argv);
+    Options options = ProcessOptions(argc, argv);
     auto str_path_in = options.path_in.string();
-    auto path_out=options.path_out;
+    auto path_out = options.path_out;
     if (boost::filesystem::create_directory(path_out))
     {
         std::cerr << "creating target directory\n";
@@ -59,33 +59,33 @@ int main(int argc, char * argv[])
     metadata["timestamp"] = get_str_time();
     metadata["path_source"] = str_path_in;
 
-    std::cerr<<"assigning ids\n";
+    std::cerr << "assigning ids\n";
     vocab.read_from_dir(str_path_in);
 
     metadata["size_corpus"] = vocab.cnt_words_processed;
     metadata["cnt_words_untrimmed"] = vocab.cnt_words;
-    
+
     vocab.reduce(options.min_frequency);
     metadata["min_frequency"] = options.min_frequency;
     metadata["cnt_words"] = vocab.cnt_words;
 
-    std::cerr<<"creating list of frequencies\n";
+    std::cerr << "creating list of frequencies\n";
 
     vocab.freq_per_id.resize(vocab.cnt_words);
     vocab.lst_id2word.resize(vocab.cnt_words);
-    std::fill (vocab.freq_per_id.begin(),vocab.freq_per_id.end(),0);   
-    std::cerr<<"populating frequencies\n";
+    std::fill (vocab.freq_per_id.begin(), vocab.freq_per_id.end(), 0);
+    std::cerr << "populating frequencies\n";
     vocab.populate_frequency();
     vocab.reassign_ids(vocab.freq_per_id);
     vocab.populate_frequency();
     vocab.populate_ids();
-    auto time_end = std::time(nullptr);    
+    auto time_end = std::time(nullptr);
     metadata["execution_time"] = time_end - time_start;
-    std::cerr<<"dumping ids and frequencies\n";
+    std::cerr << "dumping ids and frequencies\n";
 
     vocab.dump_ids((path_out / boost::filesystem::path("ids")).string());
     vocab.dump_frequency((path_out / boost::filesystem::path("frequencies")).string());
-    write_vector_to_file((path_out / boost::filesystem::path("freq_per_id")).string(),vocab.freq_per_id);
+    write_vector_to_file((path_out / boost::filesystem::path("freq_per_id")).string(), vocab.freq_per_id);
     write_value_to_file((path_out / boost::filesystem::path("metadata.json")).string(), metadata.dump(4));
 
     return 0;
